3346daa76c7e17d52dc36658779c6b1a8670964e
Finlitvin <ros.litvin@gmail.com>
2025-12-04 23:13:01 +0300

    test: добавлены тесты

---
+++ tests/conftest.py
@@ -0,0 +1,10 @@
+from typing import Generator
+
+import pytest
+
+from main import HttpClient
+
+@pytest.fixture
+def http_client() -> Generator[HttpClient]:
+    http_client = HttpClient("https://jsonplaceholder.typicode.com")
+    yield http_client
---
+++ tests/test.py
@@ -0,0 +1,9 @@
+from main import HttpClient, HTTPResponse
+
+def test_request_get(http_client: HttpClient):
+    response: HTTPResponse = http_client.request(
+        "GET",
+        "/posts/1",
+    )
+
+    assert response.status_code == 200
